c3683dc4b560861fd692ff92382b4b147e99eb6c
yanshou517 <ra89517r@gmail.com>
2024-11-25 13:42:24 +0800

    [新增]專案初始化

---
+++ gmosbook/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button type="primary">Primary Button</el-button>
+    <el-select-v2
+      v-model="value1"
+      :options="options"
+      placeholder="Please select"
+      style="width: 240px; margin-right: 16px; vertical-align: middle"
+      allow-create
+      filterable
+      multiple
+      clearable
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
