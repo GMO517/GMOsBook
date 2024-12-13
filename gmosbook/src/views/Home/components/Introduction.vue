130f86ca62a0f17f04d9ac08431e3116b65685f6
yanshou517 <ra89517r@gmail.com>
2024-12-14 04:11:55 +0800

    [更新] 個人介面RWD

--- gmosbook/src/views/Home/components/Introduction.vue
+++ gmosbook/src/views/Home/components/Introduction.vue
@@ -3,30 +3,36 @@ import headShot from "@/assets/image/headShot.jpg";
 </script>
 
 <template>
-  <el-row>
-    <el-col :span="13" class="text-block">
+  <div class="main">
+    <div class="text-block">
       <div class="title">蘇彥碩</div>
       <div class="sub-title">前端工程師</div>
       <h4>
-        我是一個前端工程師。專長於前端開發以及遊戲開發，善於快速學習並適應新功能
+        我是一個前端工程師。<br />
+        專長於前端開發以及遊戲開發，善於快速學習並適應新功能。
+        <br />
+        熟悉3D列印，歡迎找我印些酷東西。
       </h4>
-    </el-col>
-    <el-col :span="11" class="img-block">
+    </div>
+    <div class="img-block">
       <img :src="headShot" alt="" />
-    </el-col>
-  </el-row>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 $text-dark-blue: #1e3a8a;
+.main {
+  padding: 15vh;
+  display: grid;
+  justify-items: center;
+  gap: 20vh;
+}
 
 .text-block {
-  // border: 2px solid black;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding-left: 10vh;
-
   .title {
     font-size: 50px;
     font-weight: 700;
@@ -42,17 +48,31 @@ $text-dark-blue: #1e3a8a;
     font-size: 17px;
     font-weight: 600;
     color: var(--el-text-color-regular);
+    margin-bottom: 1px;
   }
 }
+
 .img-block {
-  // border: 2px solid black;
   display: flex;
-  justify-content: center;
-  align-items: center;
   overflow: hidden;
+
   img {
     width: 50vh;
     object-fit: cover;
+  }
+}
+
+@media (min-width: 768px) {
+  .main {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (max-width: 767px) {
+  .main {
+    grid-template-columns: 1fr;
+    align-items: center;
+    gap: 5vh;
   }
 }
 </style>
